b8f7162b942ef8849183c383432605fc3732795a
小島光平 <sesuna1997_winter@yahoo.co.jp>
2017-04-28 20:21:18 +0900

    Initial Commit

---
+++ keisoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keisoku
+//
+//  Created by 小島光平 on 2017/04/28.
+//  Copyright © 2017年 小島光平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
