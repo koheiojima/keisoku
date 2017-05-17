c80929d92586d422c12722473359a752442a3ee8
小島光平 <sesuna1997_winter@yahoo.co.jp>
2017-05-18 01:53:05 +0900

    修正

--- keisoku/StartViewController.swift
+++ keisoku/StartViewController.swift
@@ -11,41 +11,194 @@ import TrueScale
 
 class StartViewController: UIViewController {
     
-    //let myNativeBoundSize: CGSize = UIScreen.main.nativeBounds.size
-    
-    //変数var
-    var squares = [UIView]()
+    var bars = [UIView]()
     var labels = [UILabel]()
+    
+    //画面の横幅 (mm換算はできない)
+    var windowW: CGFloat!
+    //画面の高さ (mm換算はできない)
+    var windowH: CGFloat!
+    //1mm x 1mmの四角形
+    let rect = TSRect(x: 0, y: 0, width: 1, height: 1, unit: .mm).cgrect
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //定数 let
-        let windowW: CGFloat = self.view.bounds.width
-        let windowH: CGFloat = self.view.bounds.height
-        let rect = TSRect(x: 0, y: 95, width: 1, height: 10, unit: .mm).cgrect
-        var bar = TSRect(x: 0, y: 95, width: 0.2, height: 10, unit: .mm).cgrect
-        var labelRect = TSRect(x: 17, y: 0, width: 4, height: 4).cgrect
-        let count: Int = Int(windowW / rect.width) * 10
         
-        for i in (0 ..< count + 1) {
-            bar.origin.x = CGFloat(i) * rect.width
-            let square = UIView(frame: bar)
-            if i%10==0 {
+        windowW = self.view.bounds.width
+        windowH = self.view.bounds.height
+        
+        //下側に表示する関数を呼ぶ（今回たまたま)
+        drawRulerBottom()
+    }
+    func resetArray() {
+        for bar in bars {
+            bar.removeFromSuperview()
+        }
+        bars.removeAll()
+        for label in labels {
+            label.removeFromSuperview()
+        }
+        labels.removeAll()
+        self.view.setNeedsDisplay()
+    }
+    //左側に定規を表示する
+    func drawRulerLeftSide() {
+        //厚み0.2mmのデフォルト目盛り
+        let bar = TSRect(x: 0, y: 0, width: 10, height: 0.2, unit: .mm).cgrect
+        //目盛りのラベルのデフォルトフレーム
+        let labelRect = TSRect(x: 17, y: 0, width: 4, height: 4).cgrect
+        //目盛りを表示できる数
+        let count: Int = Int(windowH / rect.height) + 1
+        
+        //一旦前に描画した定規を消す
+        resetArray()
+        
+        for i in (0 ..< count) {
+            
+            if i % 10 == 0 { //長い目盛りの時
+                let longBar = UIView(frame: bar)
+                longBar.frame.size.width *= 1.5
+                longBar.frame.origin.y = CGFloat(i) * rect.height
+                longBar.backgroundColor = UIColor.darkGray
+                bars.append(longBar)
+                self.view.addSubview(longBar)
                 
-                square.frame.size.height *= -1.5
-                square.transform.translatedBy(x: 0, y: 100)
-                labelRect.origin.x = CGFloat(i) * rect.width
                 let label = UILabel(frame: labelRect)
                 label.text = String(Int(i/10))
-                label.frame.origin.x -= 5
+                //ここを加工して続きからできるようにする
+                label.frame.origin.x = CGFloat(i) * rect.width
+                label.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+                label.frame.origin.y -= 5
+                
                 labels.append(label)
                 self.view.addSubview(label)
+                
+            } else {//短い目盛りの時
+                let shortBar = UIView(frame: bar)
+                shortBar.frame.origin.y = CGFloat(i) * rect.height
+                shortBar.backgroundColor = UIColor.darkGray
+                bars.append(shortBar)
+                self.view.addSubview(shortBar)
             }
-            square.backgroundColor = UIColor.darkGray
-            squares.append(square)
-            self.view.addSubview(square)
         }
+        self.view.setNeedsDisplay()
     }
     
+    //下部に定規を表示する
+    func drawRulerBottom() {
+        let bar = TSRect(x: 0, y: 0, width: 0.2, height: 10, unit: .mm).cgrect
+        let labelRect = TSRect(x: 0, y: 17, width: 4, height: 4).cgrect
+        let count: Int = Int(windowW / rect.width) + 1
+        
+        resetArray()
+        
+        for i in (0 ..< count) {
+            if i % 10 == 0 { //長い目盛りの時
+                let longBar = UIView(frame: bar)
+                longBar.frame.size.height *= 1.5
+                longBar.frame.origin.x = CGFloat(i) * rect.width
+                longBar.frame.origin.y = windowH - longBar.frame.size.height
+                longBar.backgroundColor = UIColor.darkGray
+                bars.append(longBar)
+                self.view.addSubview(longBar)
+                
+                let label = UILabel(frame: labelRect)
+                label.text = String(Int(i/10))
+                //ここを加工して続きからできるようにする
+                label.frame.origin.x = CGFloat(i) * rect.width
+                label.frame.origin.y = windowH - longBar.frame.size.height
+                label.frame.origin.x += 5
+                labels.append(label)
+                self.view.addSubview(label)
+                
+            } else {
+                //短い目盛りの時
+                let shortBar = UIView(frame: bar)
+                shortBar.frame.origin.x = CGFloat(i) * rect.width
+                shortBar.frame.origin.y = windowH - shortBar.frame.size.height
+                shortBar.backgroundColor = UIColor.darkGray
+                bars.append(shortBar)
+                self.view.addSubview(shortBar)
+            }
+        }
+        self.view.setNeedsDisplay()
+    }
     
+    //右側に定規を表示する
+    func drawRulerRightSide() {
+        let bar = TSRect(x: 0, y: 0, width: 10, height: 0.2, unit: .mm).cgrect
+        let labelRect = TSRect(x: 17, y: 0, width: 4, height: 4).cgrect
+        let count: Int = Int(windowH / rect.height) + 1
+        
+        resetArray()
+        
+        for i in (0 ..< count) {
+            if i % 10 == 0 { //長い目盛りの時
+                let longBar = UIView(frame: bar)
+                longBar.frame.size.width *= 1.5
+                longBar.frame.origin.y = windowH - (CGFloat(i) * rect.height)
+                longBar.frame.origin.x = windowW - longBar.frame.size.width
+                longBar.backgroundColor = UIColor.darkGray
+                bars.append(longBar)
+                self.view.addSubview(longBar)
+                
+                let label = UILabel(frame: labelRect)
+                label.text = String(Int(i/10)) //ここを加工して続きからできるようにする
+                label.frame.origin.y = windowH - (CGFloat(i) * rect.height)
+                label.transform = CGAffineTransform(rotationAngle: -CGFloat(Double.pi / 2))
+                label.frame.origin.y -= 5
+                
+                labels.append(label)
+                self.view.addSubview(label)
+                
+            } else { //短い目盛りの時
+                let shortBar = UIView(frame: bar)
+                shortBar.frame.origin.y = windowH - (CGFloat(i) * rect.height)
+                shortBar.frame.origin.x = windowW - shortBar.frame.size.width
+                shortBar.backgroundColor = UIColor.darkGray
+                bars.append(shortBar)
+                self.view.addSubview(shortBar)
+            }
+        }
+        self.view.setNeedsDisplay()
+    }
+    
+    //上部に定規を表示する
+    func drawRulerTop() {
+        let bar = TSRect(x: 0, y: 0, width: 0.2, height: 10, unit: .mm).cgrect
+        let labelRect = TSRect(x: 0, y: 17, width: 4, height: 4).cgrect
+        let count: Int = Int(windowW / rect.width) + 1
+        
+        resetArray()
+        
+        for i in (0 ..< count) {
+            if i % 10 == 0 { //長い目盛りの時
+                let longBar = UIView(frame: bar)
+                longBar.frame.size.height *= 1.5
+                longBar.frame.origin.x = windowW - (CGFloat(i) * rect.width)
+                longBar.backgroundColor = UIColor.darkGray
+                bars.append(longBar)
+                self.view.addSubview(longBar)
+                
+                let label = UILabel(frame: labelRect)
+                label.text = String(Int(i/10))
+                //ここを加工して続きからできるようにする
+                label.frame.origin.x = windowW - (CGFloat(i) * rect.width)
+                label.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+                label.frame.origin.x += 5
+                
+                labels.append(label)
+                self.view.addSubview(label)
+                
+            } else {
+                //短い目盛りの時
+                let shortBar = UIView(frame: bar)
+                shortBar.frame.origin.x = windowW - (CGFloat(i) * rect.width)
+                shortBar.backgroundColor = UIColor.darkGray
+                bars.append(shortBar)
+                self.view.addSubview(shortBar)
+            }
+        }
+        self.view.setNeedsDisplay()
+    }
 }
